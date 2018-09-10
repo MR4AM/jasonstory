00bc158ba876e4d548e3fc7145b76142aa2857ba
limenghui <1479856243@qq.com>
2018-09-10 19:55:33 +0800

    1955

--- pages/dadishu/dadishu.js
+++ pages/dadishu/dadishu.js
@@ -7,11 +7,19 @@ Page({
     start:false,
     count:3,
     setInter:'',
-    timer:'',
+    timer2:'',
+    timer3:'',
     tips:'Ready!',
     masknone:false,
     activeidx:'',
-    score:0,
+    score:0,//当前关卡得分
+    gamestate:1,//游戏关卡,
+    gamecountdown:10,//游戏当前关卡倒计时,
+    gameover:false,
+    pauge:false,//游戏中断中状态，
+    needpoint:0,//所需分数,
+    btntext:'',//游戏控制按钮,
+    passwords:['你怎么那么腻害','被你的手速折服','大哥，请喝茶','kidding me?']
   },
 
   readygame(){
@@ -32,21 +40,68 @@ Page({
             that.setData({
               masknone:true,
             })
-            this.randomflash(1000);
+            this.gamestart(1000,30,10)
           },2000)
         }
       })
     }, 1000)
   },
+  //游戏开始函数，三个参数分别是地鼠闪跳时间间隔，关卡倒计时，关卡所需分数
+  gamestart(duration,countdown,score){
+    //清除定时器
+    clearInterval(this.data.setInter)
+    clearInterval(this.data.timer2);
+    clearInterval(this.data.timer3);
+    //游戏开始，倒计时开始，动画开始
+    this.randomflash(duration);
+    this.setData({
+      gamecountdown:countdown
+    },()=>{
+      this.setData({
+        needpoint:score
+      },()=>{
+        this.gamecountdown(this.data.needpoint);
+      })
+    })
+  },
   //地鼠随机出现
-  randomflash(duration){
+  randomflash(duration,stop){
     let that= this;
     that.data.timer2=setInterval(()=>{
         let rannum=Math.floor(Math.random()*12);
         that.setData({
           activeidx:rannum,
+        },()=>{
+          if (that.data.gameover){
+            clearInterval(that.data.timer2)
+          }
         })
     },duration)
+  },
+  //游戏当前倒计时
+  gamecountdown(score){
+    let that =this;
+      that.data.timer3=setInterval(()=>{
+        that.setData({
+          gamecountdown:that.data.gamecountdown-1
+        },()=>{
+          if (that.data.gamecountdown < 0 && this.data.score < score) {
+            clearInterval(that.data.timer3);
+            that.setData({
+              gameover:true
+            })
+            this.setData({
+              gamestate:1,
+              pauge: true,
+              masknone: false,
+              tips: '很遗憾，你在规定时间内没有打到对应分数',
+              count: '',
+              btntext:'再来一局',
+              score: 0
+            })
+          }
+        })
+      },1000)
   },
   //打击地鼠
   hitmice(e){
@@ -55,8 +110,37 @@ Page({
       console.log(e.currentTarget.dataset.hitidx,'打中了');
       this.setData({
         score:this.data.score+1,
+      },()=>{
+        if(this.data.score>=this.data.needpoint){
+          this.setData({
+            gamestate: this.data.gamestate + 1,
+            pauge:true,
+            masknone:false,
+            tips:this.data.passwords[this.data.gamestate-2],
+            count:'',
+            btntext: '下一关',
+            score: 0
+          })
+        }
       })
     }
+  },
+  //到下一关
+  tonextstate(){
+    switch(this.data.gamestate){
+      case 2:
+      this.gamestart(1000, 30, 20);
+      break;
+      case 3:
+      this.gamestart(900, 30, 30);
+      break;
+      case 4:
+      this.gamestart(800, 30, 30);
+      break;
+    }
+    this.setData({
+      masknone:true,
+    })
   },
   /**
    * 生命周期函数--监听页面加载
